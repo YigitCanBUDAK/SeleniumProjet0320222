00f63d50a2204ce5b0b40ac82f8d3f0a2dfda15a
YigitCanBUDAK <budakyigitcan26@gmail.com>
2022-03-20 02:33:45 +0300

    Initial commit

---
+++ src/sampleapp/HelloWorld.java
@@ -0,0 +1,24 @@
+package sampleapp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorld
+
+{
+    public static void main(String[] args)
+    {
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\can\\Desktop\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+        driver.quit();
+
+
+
+
+
+    }
+
+
+}
